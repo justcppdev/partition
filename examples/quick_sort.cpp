b295e49ee20b54e78804e8c862af7f313297825d
justcppdeveloper <justcppdeveloper@ya.ru>
2017-10-02 19:49:14 +0300

    added quick sort as example

---
+++ examples/quick_sort.cpp
@@ -0,0 +1,32 @@
+#include "hoare_partition.hpp"
+#include "lomuto_partition.hpp"
+
+template <typename _ForwardIterator>
+auto quick_sort_using_lomuto_partition( _ForwardIterator first, _ForwardIterator last )
+{
+}
+
+template <typename _BidirectionalIterator>
+auto quick_sort_using_lomuto_partition( _BidirectionalIterator first, _BidirectionalIterator last )
+{
+    if( first != last ) {
+        auto partition = lomuto_partition( first, last, *first );
+        quick_sort_using_lomuto_partition( first, partition.equal.begin );
+        quick_sort_using_lomuto_partition( partition.great.begin, last );
+    }
+}
+
+int main()
+{
+    for( std::string line; std::getline( std::cin, line ); ) {
+        std::stringstream stream{ line };
+        std::vector<int> numbers;
+        std::copy( std::istream_iterator<int>{ stream }, {}, std::back_inserter( numbers ) );
+
+        quick_sort_using_lomuto_partition( numbers.begin(), numbers.end() );
+
+        std::copy( numbers.cbegin(), numbers.cend(), std::ostream_iterator<int>{ std::cout, " " } );
+    }
+
+    return 0;
+}
